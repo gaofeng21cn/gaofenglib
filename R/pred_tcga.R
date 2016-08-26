3b49116ab5818c38416f01c8dca65894961704ef
gaofeng21cn <gaofeng21cn@hotmail.com>
2016-08-26 16:59:17 +0800

    add new clinical info

--- R/pred_tcga.R
+++ R/pred_tcga.R
@@ -24,7 +24,7 @@ extract_tcga_clinical <- function(clin) {
   relapse <- (labels == "yes")
   names(relapse) <- names(labels)
 
-  ## Time
+  ## Relapse Time
   t <- c("patient.follow_ups.follow_up.new_tumor_events.new_tumor_event.days_to_new_tumor_event_after_initial_treatment",
          "patient.follow_ups.follow_up.days_to_death",
          "patient.follow_ups.follow_up.days_to_last_followup"
@@ -35,8 +35,27 @@ extract_tcga_clinical <- function(clin) {
   time <- tmp[, 1]
   time[is.na(time)] <- as.vector(t(tmp[is.na(time), 2]))
   time[is.na(time)] <- as.vector(t(tmp[is.na(time), 3]))
-  time <- as.numeric(time)
-  names(time) <- toupper(clin[, "patient.bcr_patient_barcode"])
+  relapse.time <- as.numeric(time)
+  names(relapse.time) <- toupper(clin[, "patient.bcr_patient_barcode"])
 
-  data.frame(stage, relapse, time)
+  ## Death
+  labels <- clin[, "patient.vital_status"]
+  names(labels) <- toupper(clin[, "patient.bcr_patient_barcode"])
+
+  death <- (labels == "dead")
+  names(death) <- names(labels)
+
+  ## Death time
+  t <- c("patient.follow_ups.follow_up.days_to_death", "patient.follow_ups.follow_up.days_to_last_followup")
+  tmp <- clin[, t]
+  colnames(tmp) <- NULL
+
+  time <- tmp[, 1]
+  time[is.na(time)] <- as.vector(t(tmp[is.na(time), 2]))
+  # time[is.na(time)] <- as.vector(t(tmp[is.na(time), 3]))
+  death.time <- as.numeric(time)
+  names(death.time) <- toupper(clin[, "patient.bcr_patient_barcode"])
+
+
+  data.frame(stage, relapse, relapse.time, death, death.time)
 }
