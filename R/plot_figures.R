14625613022fa204d08aafba8c5bd7c19e2a5a77
gaofeng21cn <gaofeng21cn@hotmail.com>
2016-08-26 20:52:10 +0800

    fix color name

--- R/plot_figures.R
+++ R/plot_figures.R
@@ -30,13 +30,9 @@ plot_KMCurve <- function(clinical, labels, annot = NULL, color = NULL, font = "A
     }
     color <- color[1:length(unique(na.omit(labels)))]
 
-    if(class(labels) == "factor") {
-      names(color) <- levels(labels)
-    } else {
-      names(color) <- unique(na.omit(labels))
-    }
-
+    names(color) <-  ifelse(class(labels) == "factor", levels(labels), unique(na.omit(labels)))
   }
+  if(!is.null(names(color))) color <- color[ifelse(class(labels) == "factor", levels(labels), unique(na.omit(labels)))]
 
   p <- GGally::ggsurv(surv, surv.col = color, xlab = xlab, ylab = ylab) +
     annotate("text", family=font, x = Inf, y = Inf, label =
